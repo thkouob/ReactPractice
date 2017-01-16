12c5854d6e756cb0dddfac07c19de5e5b96734d2
sc9j <Stella.W.Chen@newegg.com>
2017-01-16 17:35:08 +0800

    Handling Events

--- ReactPractice/Controllers/HomeController.cs
+++ ReactPractice/Controllers/HomeController.cs
@@ -43,5 +43,10 @@ namespace ReactPractice.Controllers
         {
             return View();
         }
+
+        public ActionResult DemoHandlingEvents()
+        {
+            return View();
+        }
     }
 }
